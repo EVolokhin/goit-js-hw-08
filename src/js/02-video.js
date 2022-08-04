b724ffc07f53f9d5c4327852e73d91ec3d0be302
Evolokhin <evolokhin@rightandabove.com>
2022-08-04 08:59:12 +0300

    - add 2 task videoPlayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import _ from 'lodash';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localKey = 'videoplayer-current-time';
+
+const durationCallback = ({ seconds }) => {
+  localStorage.setItem(localKey, seconds);
+};
+
+const setPlayerTime = () => {
+  if (!localStorage.getItem(localKey)) {
+    return;
+  }
+  const currentVideoTime = localStorage.getItem(localKey);
+  player
+    .setCurrentTime(currentVideoTime)
+    .then(() => {
+      player.play();
+    })
+    .catch(error => {
+      switch (error.name) {
+        case 'RangeError':
+          alert(
+            'The time was less than 0 or greater than the video’s duration'
+          );
+          break;
+        default:
+          alert('Player Error');
+          break;
+      }
+    });
+};
+
+window.addEventListener('load', setPlayerTime);
+player.on('timeupdate', _.throttle(durationCallback, 1000));
